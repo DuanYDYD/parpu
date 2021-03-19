f0f0d3f5181ef340bec6e92b14dd2cc22acabd71
Xu Tian Liang <xutianliang128@126.com>
2021-03-19 23:14:15 +0800

    complete the models

--- parpu/models.py
+++ parpu/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class User(models.Model):
     '''用户表'''
+    # administrater 要继承
 
     gender = (
         ('male', '男'),
@@ -26,11 +27,13 @@ class User(models.Model):
     password = models.CharField(max_length=256)
     email = models.EmailField(unique=True)
     sex = models.CharField(max_length=32, choices=gender, default='男')
-    intestedarea = models.CharField(max_length=128, choices=area, default='shangsai')
+    intestedarea = models.CharField(max_length=128, choices=area, default='sports')
     major = models.CharField(max_length=128, choices=majorchoice)
     motto = models.CharField(max_length=128)
     graduationyear = models.DateField()
     c_time = models.DateTimeField(auto_now_add=True)
+    #成就recording
+    #对自己的评级
 
     def __str__(self):
         return self.name
@@ -42,9 +45,89 @@ class User(models.Model):
 
 class Team(models.Model):
     '''队伍'''
+    resultc=(
+        ('fw','first award')
+        ('sw','second award')
+        ('tw','third award')
+        ('aw','award')
+    )
 
+    area = (
+        ('co', 'commercial contest'),
+        ('sp', 'sports'),
+    )
 
     tname = models.CharField(max_length=128)
     tid = models.AutoField(primary_key=True)
     leader = models.ForeignKey('User', on_delete=models.CASCADE)
-    teaMem =
+    #teamembers = models.  #队友
+    tannounce = models.TextField() #验证
+    tresult = models.CharField(max_length=32, choices=resultc)
+    contestbelonging = models.CharField(max_length=128, choices=area, default='sports')
+    teamrequirement = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '队伍'
+        verbose_name_plural = '队伍'
+
+class community(models.Model):
+    '''社区'''
+
+    area = (
+        ('co', 'commercial contest'),
+        ('sp', 'sports'),
+    )
+
+    commarea = models.CharField(max_length=128, choices=area, default='sports')
+    topic = models.CharField(max_length=128)
+    commid = models.AutoField(primary_key=True)
+    #list
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '社区'
+        verbose_name_plural = '社区'
+
+class post(models.Model):
+    '''帖子'''
+
+    pid = models.AutoField(primary_key=True)
+    content = models.TextField()
+    comment = models.TextField() #要做多个
+    postlikenum = models.IntegerField(default=0)
+    commentlikenum = models.IntegerField(default=0)
+    binfo = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '帖子'
+        verbose_name_plural = '帖子'
+
+class contest(models.Model):
+    '''比赛'''
+
+    resultc = (
+        ('fw', 'first award')
+        ('sw', 'second award')
+        ('tw', 'third award')
+        ('aw', 'award')
+    )
+
+    area = (
+        ('co', 'commercial contest'),
+        ('sp', 'sports'),
+    )
+
+    cname = models.CharField(max_length=128)
+    cid = models.AutoField(primary_key=True)
+    crequirement = models.TextField()
+    con_c_time = models.DateTimeField(auto_now_add=True)
+    conaward = models.CharField(max_length=32, choices=resultc)
+    contestcat = models.CharField(max_length=128, choices=area, default='sports')
