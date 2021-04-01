c53340b5f523957fa216b296bafab7d573e35d00
Xu Tian Liang <xutianliang128@126.com>
2021-04-01 23:11:54 +0800

    complete the models

--- parpu/models.py
+++ parpu/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
+from django.db.models import signals
+import datetime
 
 
-class User(models.Model):
+
+class User(AbstractUser):
     '''用户表'''
     # administrater 要继承
 
@@ -22,10 +29,9 @@ class User(models.Model):
         ('cs','computer science')
     )
 
-    name = models.CharField(max_length=128)
-    id = models.AutoField(primary_key=True)
-    password = models.CharField(max_length=256)
-    email = models.EmailField(unique=True)
+    nickname = models.CharField(max_length=128, blank=True, null=True)
+    friends = models.ManyToManyField(
+        'self', blank=True, null=True, related_name='friends')
     sex = models.CharField(max_length=32, choices=gender, default='男')
     intestedarea = models.CharField(max_length=128, choices=area, default='sports')
     major = models.CharField(max_length=128, choices=majorchoice)
@@ -36,12 +42,18 @@ class User(models.Model):
     #对自己的评级
 
     def __str__(self):
-        return self.name
+        return self.username
 
     class Meta:
         ordering = ['c_time']
         verbose_name = '用户'
         verbose_name_plural = '用户'
+
+    def checkfriend(self, username):
+        if username in self.friends.all():
+            return True
+        else:
+            return False
 
 class Team(models.Model):
     '''队伍'''
@@ -61,13 +73,15 @@ class Team(models.Model):
     tid = models.AutoField(primary_key=True)
     leader = models.ForeignKey('User', on_delete=models.CASCADE)
     #teamembers = models.  #队友
+    teamembers = models.ManyToManyField(
+        'self', blank=True, null=True, related_name='teamembers')
     tannounce = models.TextField() #验证
     tresult = models.CharField(max_length=32, choices=resultc)
     contestbelonging = models.CharField(max_length=128, choices=area, default='sports')
     teamrequirement = models.TextField()
 
     def __str__(self):
-        return self.name
+        return self.tname
 
     class Meta:
         verbose_name = '队伍'
@@ -87,7 +101,7 @@ class community(models.Model):
     #list
 
     def __str__(self):
-        return self.name
+        return self.name###################
 
     class Meta:
         verbose_name = '社区'
@@ -104,11 +118,43 @@ class post(models.Model):
     binfo = models.TextField()
 
     def __str__(self):
-        return self.name
+        return self.name#######
 
     class Meta:
         verbose_name = '帖子'
         verbose_name_plural = '帖子'
+
+
+
+class Post(models.Model):  #文章
+    title = models.CharField(max_length=30)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='post_author')  #作者
+    column = models.ForeignKey(Column)  #所属板块 #################
+    type_name = models.ForeignKey(PostType)  #文章类型 ###################
+    content = models.TextField()
+
+    view_times = models.IntegerField(default=0)  #浏览次数
+    responce_times = models.IntegerField(default=0)  #回复次数
+    last_response = models.ForeignKey(settings.AUTH_USER_MODEL)  #最后回复者
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'post'
+        ordering = ['-created_at']
+        verbose_name_plural = u'主题'
+
+    def __unicode__(self):
+        return self.title
+
+    def description(self):
+        return u'%s 发表了主题《%s》' % (self.author, self.title)
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('post_detail', (), {'post_pk': self.pk})
 
 class contest(models.Model):
     '''比赛'''
@@ -131,3 +177,31 @@ class contest(models.Model):
     con_c_time = models.DateTimeField(auto_now_add=True)
     conaward = models.CharField(max_length=32, choices=resultc)
     contestcat = models.CharField(max_length=128, choices=area, default='sports')
+
+
+
+class Comment(models.Model):  #评论
+    post = models.ForeignKey(Post)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    comment_parent = models.ForeignKey(
+        'self', blank=True, null=True, related_name='childcomment')
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'comment'
+        ordering = ['created_at']
+        verbose_name_plural = u'评论'
+
+    def __unicode__(self):
+        return self.content
+
+    def description(self):
+        return u'%s 回复了您的帖子(%s) R:《%s》' % (self.author, self.post,
+                                           self.content)
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('post_detail', (), {'post_pk': self.post.pk})
