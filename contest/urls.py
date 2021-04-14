a333ad94ac584eae5c90d8d454d1135ffc076aac
Zijian DUAN <1155107769@link.cuhk.edu.hk>
2021-04-14 15:17:03 +0800

    contest completed

--- contest/urls.py
+++ contest/urls.py
@@ -7,5 +7,6 @@ app_name = 'contest'
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('contestpage/', views.contestpage, name='info'),
-    path('addpost/', views.addpost, name='newpost')
+    path('addpost/', views.addpost, name='newpost'),
+    path('search/', views.search, name='search'),
 ]
