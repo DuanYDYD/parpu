f67e1117d9aa1a001527b327c45a75691f75f9d7
Zijian DUAN <1155107769@link.cuhk.edu.hk>
2021-04-14 14:02:37 +0800

    04/14

--- contest/views.py
+++ contest/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import get_object_or_404, render
 from contest.models import Contest
-import time
+from django.db.models import Max, Min
+from forum.models import Post, Column
+from django.db import models
 
 def index(request):
     context = {}
     recent_contest = Contest.objects.order_by("-id")[0:6]
-    return render(request, "ContestSearch.html", {"recent": recent_contest})
+    con_number = Contest.objects.all().aggregate(Max('id'))
+    recent_post = Post.objects.order_by("-id")[0:3]
+    return render(request, "ContestSearch.html", {"recent": recent_contest, "contest_number": con_number['id__max'],
+                                                  "post": recent_post})
 
 def contestpage(request):
     contest_id = request.GET.get('contest_id')
